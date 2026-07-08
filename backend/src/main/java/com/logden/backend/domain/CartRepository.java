0ecf54d49a0387ebebfac111708d87b703fac247
Eriikakl <148944749+Eriikakl@users.noreply.github.com>
2026-07-08 19:45:14 +0300

    Add method to find cart by user

--- backend/src/main/java/com/logden/backend/domain/CartRepository.java
+++ backend/src/main/java/com/logden/backend/domain/CartRepository.java
@@ -1,7 +1,10 @@
 package com.logden.backend.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
+    Optional<Cart> findByUser(User user);
 }
