e49e512f649265266891869412c6abd587285c7d
Eriikakl <148944749+Eriikakl@users.noreply.github.com>
2026-07-08 21:24:46 +0300

    Comment out cart and order test data

--- backend/src/main/java/com/logden/backend/BackendApplication.java
+++ backend/src/main/java/com/logden/backend/BackendApplication.java
@@ -26,61 +26,62 @@ import com.logden.backend.domain.UserRepository;
 @SpringBootApplication
 public class BackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(BackendApplication.class, args);
+  }
 
-    @Bean
-    CommandLineRunner loadData(ProductRepository productRepository,
-            CategoryRepository categoryRepository,
-            UserRepository userRepository,
-            CartRepository cartRepository,
-            CartItemRepository cartItemRepository,
-            OrderRepository orderRepository,
-            OrderItemRepository orderItemRepository) {
-        return args -> {
+  @Bean
+  CommandLineRunner loadData(ProductRepository productRepository,
+      CategoryRepository categoryRepository,
+      UserRepository userRepository,
+      CartRepository cartRepository,
+      CartItemRepository cartItemRepository,
+      OrderRepository orderRepository,
+      OrderItemRepository orderItemRepository) {
+    return args -> {
 
-            User user = userRepository.save(new User(
-                    "Erkki",
-                    "Esimerkki",
-                    "Testikatu 1, Helsinki",
-                    "erkki@example.com",
-                    "0401234567",
-                    "salasana",
-                    "CUSTOMER"));
+      User user = userRepository.save(new User(
+          "Erkki",
+          "Esimerkki",
+          "Testikatu 1, Helsinki",
+          "erkki@example.com",
+          "0401234567",
+          "salasana",
+          "CUSTOMER"));
 
-            Cart cart = cartRepository.save(new Cart(user));
+      Cart cart = cartRepository.save(new Cart(user));
 
-            Category decoration = new Category("Puukoristeet", "Koristeeksi luotuja puuesineitä");
-            categoryRepository.save(decoration);
+      Category decoration = new Category("Puukoristeet", "Koristeeksi luotuja puuesineitä");
+      categoryRepository.save(decoration);
 
-            Product product1 = productRepository.save(new Product(
-                    "Puulehmä",
-                    "Vuoltu puulehmä",
-                    new BigDecimal("299.90"),
-                    20,
-                    true,
-                    decoration));
+      Product product1 = productRepository.save(new Product(
+          "Puulehmä",
+          "Vuoltu puulehmä",
+          new BigDecimal("299.90"),
+          20,
+          true,
+          decoration));
 
-            Product product2 = productRepository.save(new Product(
-                    "Puusaapas",
-                    "Pölkystä muotoiltu saapas",
-                    new BigDecimal("499.90"),
-                    15,
-                    true,
-                    decoration));
+      Product product2 = productRepository.save(new Product(
+          "Puusaapas",
+          "Pölkystä muotoiltu saapas",
+          new BigDecimal("499.90"),
+          15,
+          true,
+          decoration));
 
-            cartItemRepository.save(new CartItem(cart, product1, 2));
-            cartItemRepository.save(new CartItem(cart, product2, 1));
+      // cartItemRepository.save(new CartItem(cart, product1, 2));
+      // cartItemRepository.save(new CartItem(cart, product2, 1));
+      //
+      // Order order = orderRepository.save(new Order(user, "PAID",
+      // LocalDateTime.now(), new BigDecimal("1099.70")));
+      // orderItemRepository.save(new OrderItem(
+      // order, product1, 2, product1.getPrice()));
+      //
+      // orderItemRepository.save(new OrderItem(
+      // order, product2, 1, product2.getPrice()));
 
-            Order order = orderRepository.save(new Order(user, "PAID", LocalDateTime.now(), new BigDecimal("1099.70")));
-            orderItemRepository.save(new OrderItem(
-                    order, product1, 2, product1.getPrice()));
-
-            orderItemRepository.save(new OrderItem(
-                    order, product2, 1, product2.getPrice()));
-
-        };
-    }
+    };
+  }
 
 }
