457c107fcee6929fbf1211c7700e8e97c687c080
Eriikakl <148944749+Eriikakl@users.noreply.github.com>
2026-07-08 15:52:59 +0300

    Add method to find cart item by cart and product

--- backend/src/main/java/com/logden/backend/domain/CartItemRepository.java
+++ backend/src/main/java/com/logden/backend/domain/CartItemRepository.java
@@ -1,7 +1,10 @@
 package com.logden.backend.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
+    Optional<CartItem> findByCartAndProduct(Cart cart, Product product);
 }
