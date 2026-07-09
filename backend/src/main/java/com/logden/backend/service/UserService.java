538a34e9e0acf10b29d0d7136bc3b1d703d4b1cc
Eriikakl <148944749+Eriikakl@users.noreply.github.com>
2026-07-09 23:22:23 +0300

    Add user CRUD operations

--- backend/src/main/java/com/logden/backend/service/UserService.java
+++ backend/src/main/java/com/logden/backend/service/UserService.java
@@ -1,5 +1,7 @@
 package com.logden.backend.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.logden.backend.domain.User;
@@ -8,7 +10,7 @@ import com.logden.backend.domain.UserRepository;
 @Service
 public class UserService {
 
-     private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -19,7 +21,37 @@ public class UserService {
                 .orElseThrow(() -> new RuntimeException("User not found"));
     }
 
-    public User saveUser(User user) {
+    public User addUser(User user) {
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new RuntimeException("Email already exists");
+        }
         return userRepository.save(user);
+    }
+
+    public User updateUser(Long id, User updatedUser) {
+
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        user.setFirstName(updatedUser.getFirstName());
+        user.setLastName(updatedUser.getLastName());
+        user.setAddress(updatedUser.getAddress());
+        user.setEmail(updatedUser.getEmail());
+        user.setPhone(updatedUser.getPhone());
+        user.setPasswordHash(updatedUser.getPasswordHash());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        userRepository.delete(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
