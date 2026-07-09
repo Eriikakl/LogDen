2fc3a2c5ad5bd97d245527ec34458f628e0afb0d
Eriikakl <148944749+Eriikakl@users.noreply.github.com>
2026-07-09 23:20:49 +0300

    Add findByEmail to UserRepository

--- backend/src/main/java/com/logden/backend/domain/UserRepository.java
+++ backend/src/main/java/com/logden/backend/domain/UserRepository.java
@@ -1,7 +1,11 @@
 package com.logden.backend.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 
 }
