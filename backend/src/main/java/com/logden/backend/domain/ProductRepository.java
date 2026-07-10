ea5cc080d2a518dcfdc285c358715599ba6f4a69
Eriikakl <148944749+Eriikakl@users.noreply.github.com>
2026-07-11 00:34:25 +0300

    Add findByName query for products

--- backend/src/main/java/com/logden/backend/domain/ProductRepository.java
+++ backend/src/main/java/com/logden/backend/domain/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.logden.backend.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByName(String name);
 
 }
