b5a308310b75aa484359f2df95b296520544dc09
Eriikakl <148944749+Eriikakl@users.noreply.github.com>
2026-07-10 22:33:56 +0300

    Use custom exceptions in CategoryService

--- backend/src/main/java/com/logden/backend/service/CategoryService.java
+++ backend/src/main/java/com/logden/backend/service/CategoryService.java
@@ -1,11 +1,14 @@
 package com.logden.backend.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.logden.backend.domain.Category;
 import com.logden.backend.domain.CategoryRepository;
+import com.logden.backend.exception.ResourceAlreadyExistsException;
+import com.logden.backend.exception.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -22,20 +25,27 @@ public class CategoryService {
 
     public Category getCategoryById(Long id) {
         return categoryRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Category not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found"));
     }
 
     // Adminille endpointit kategorian lisäykseen, muokkaukseen ja poistoon
 
     public Category addCategory(Category category) {
+        if (categoryRepository.findByName(category.getName()).isPresent()) {
+            throw new ResourceAlreadyExistsException("Category name already exists");
+        }
         return categoryRepository.save(category);
     }
 
     public Category updateCategory(Long id, Category updatedCategory) {
 
         Category category = categoryRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Category not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found"));
 
+        Optional<Category> existingCategory = categoryRepository.findByName(updatedCategory.getName());
+        if (existingCategory.isPresent() && !existingCategory.get().getCategoryId().equals(id)) {
+            throw new ResourceAlreadyExistsException("Category name already exists");
+        }
         category.setName(updatedCategory.getName());
         category.setDescription(updatedCategory.getDescription());
 
@@ -44,7 +54,7 @@ public class CategoryService {
 
     public void deleteCategory(Long id) {
         if (!categoryRepository.existsById(id)) {
-            throw new RuntimeException("Category not found");
+            throw new ResourceNotFoundException("Category not found");
         }
         categoryRepository.deleteById(id);
     }
