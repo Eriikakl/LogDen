3fe262b982591feb74ee33a7c5212c6229e7e891
Eriikakl <148944749+Eriikakl@users.noreply.github.com>
2026-07-10 22:32:49 +0300

    Add findByName query for categories

--- backend/src/main/java/com/logden/backend/domain/CategoryRepository.java
+++ backend/src/main/java/com/logden/backend/domain/CategoryRepository.java
@@ -1,7 +1,10 @@
 package com.logden.backend.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
 
 }
