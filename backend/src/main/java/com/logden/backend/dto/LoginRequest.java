36589c1229d49514e276f42ee1eb47a3608680e7
Eriikakl <148944749+Eriikakl@users.noreply.github.com>
2026-07-30 23:10:08 +0300

    Add login request and response DTOs

---
+++ backend/src/main/java/com/logden/backend/dto/LoginRequest.java
@@ -0,0 +1,39 @@
+package com.logden.backend.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class LoginRequest {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    public LoginRequest(@NotBlank @Email String email, @NotBlank String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public LoginRequest() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
