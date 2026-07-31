73c4e9a0a68a7494ce1d879e6c2432392c4c243e
Eriikakl <148944749+Eriikakl@users.noreply.github.com>
2026-07-31 14:00:20 +0300

    add JWT token parsing and validation

--- backend/src/main/java/com/logden/backend/security/JwtService.java
+++ backend/src/main/java/com/logden/backend/security/JwtService.java
@@ -1,9 +1,11 @@
 package com.logden.backend.security;
 
-import java.security.Key;
 import java.util.Date;
 
+import javax.crypto.SecretKey;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import com.logden.backend.domain.User;
@@ -33,8 +35,35 @@ public class JwtService {
 
     }
 
-    private Key getSigningKey() {
+    private SecretKey getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public Date extractExpiration(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration();
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        return (extractUsername(token).equals(userDetails.getUsername()) && !isTokenExpired(token));
+
     }
 }
